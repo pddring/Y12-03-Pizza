eeaf72a1788f1c94a3a73d52ef94244ba8ba90c2
fulfordcs <fulfordcs@gmail.com>
2022-09-23 15:10:49 +0100

    Added working validation

--- Pizza/Program.cs
+++ Pizza/Program.cs
@@ -10,6 +10,44 @@ namespace Pizza
     {
         static void Main(string[] args)
         {
+            List<string> toppings = new List<string> {
+                "Pepperoni", "Margherita", "Ham and pineapple"
+            };
+
+            string selection = ChooseFrom(toppings);
+            Console.WriteLine($"You have chosen {selection}");
+        }
+
+        private static string ChooseFrom(List<string> options)
+        {
+            Console.WriteLine("Please choose one of the following:");
+            
+
+            // repeat until user input is valid
+            bool valid = false;
+            int chosenPosition = -1;
+
+            while (!valid) {
+                // display the options
+                for (int i = 0; i < options.Count; i++) 
+                {
+                    Console.WriteLine($"{i + 1}: {options[i]}");
+                }
+
+                // ask for user input
+                Console.Write($"Choose a number (1-{options.Count}): ");
+                string response = Console.ReadLine();
+                
+                if(int.TryParse(response, out chosenPosition) &&
+                    chosenPosition > 0 &&
+                    chosenPosition <= options.Count)
+                {
+                    valid = true;
+                }
+            }
+
+            // return a valid response
+            return options[chosenPosition - 1];
         }
     }
 }
